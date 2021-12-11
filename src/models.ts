2d6cdb1a19749a0598c42d601d2283c3df34c7ab
Saicharan S <saicharans@C02DW6P2MD6R.local>
2021-12-11 19:16:33 +0530

    update application models

--- src/models.ts
+++ src/models.ts
@@ -1,21 +1,24 @@
 export interface User {
     id: string;
-    details: UserDetails
+    details: UserDetails;
 }
 
 export interface UserDetails {
     name: string;
     number: number;
-    posts: Post[];
+    articles: Article[];
     subscribers: string[];
+    isExpectingArticle: boolean;
 }
 
-export interface Post {
+export interface Article {
+    id: string;
     text: string;
-    media: string;
+    mediaUrl: string[];
     timestamp: number;
+    confirmed: boolean;
 }
 
-export const isAuthor = (user: UserDetails): boolean => {
-    return user.posts.length > 0;
+export const isAuthor = (user: User): boolean => {
+    return user.details.subscribers.length > 0;
 }
